f9791201b0a582d539d47f832ad6831ab5a77eb4
MTT-QA-Tasks <85492023+TasksForSecondCourse@users.noreply.github.com>
2021-06-10 19:36:44 +0300

    FirstTestCommitForBankQA
    CommitFile

---
+++ QA_Homework6_CommitFile/QA_Homework6_CommitFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QA_Homework6_CommitFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
